639ecd2cf5021de1f8ba8ac43927abd30ca87ea0
JulieGunawan <julie.gunawan@protonmail.com>
2025-02-15 15:57:08 -0700

    fix: eslint without formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,15 @@
-import eslint from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import tseslint from 'typescript-eslint'
-import stylistic from '@stylistic/eslint-plugin'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  eslint.configs.recommended,
-  tseslint.configs.base,
-  stylistic.configs['recommended-flat'],
-  {
-    files: ['**/*.vue'],
-    plugins: {
-      vue: pluginVue,
-    },
-    rules: {
-      ...pluginVue.configs.recommended.rules,
-    },
-  },
-  // Typescript configuration
-  {
-    name: 'typescript',
-    files: ['**/*.{ts,tsx,mts,cts}'],
-    rules: {
-      ...tseslint.configs.eslintRecommended.rules,
-      '@typescript-eslint/ban-ts-comment': 'error',
-      'no-array-constructor': 'off',
-      '@typescript-eslint/no-array-constructor': 'error',
-      '@typescript-eslint/no-duplicate-enum-values': 'error',
-      '@typescript-eslint/no-empty-object-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-extra-non-null-assertion': 'error',
-      '@typescript-eslint/no-misused-new': 'error',
-      '@typescript-eslint/no-namespace': 'error',
-      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-      '@typescript-eslint/no-require-imports': 'error',
-      '@typescript-eslint/no-this-alias': 'error',
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
-      '@typescript-eslint/no-unsafe-function-type': 'error',
-      'no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-expressions': 'error',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-wrapper-object-types': 'error',
-      '@typescript-eslint/prefer-as-const': 'error',
-      '@typescript-eslint/prefer-namespace-keyword': 'error',
-      '@typescript-eslint/triple-slash-reference': 'error',
-    },
-  },
-  {
-    name: 'app/files-to-ignore',
-    ignores: [
-      'node_modules/**',
-      '**/dist/**',
-      '**/dist-ssr/**',
-      '**/coverage/**',
-      '.git/**',
-    ],
-  },
-]
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+];
